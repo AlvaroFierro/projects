a6f09fec9fe852cd8fbc2df8069356a2a6a8a75a
Alvaro Fierro <business.afierro@gmail.com>
2022-08-01 17:24:49 -0500

    Update files

---
+++ scrimba/black-jack/arrays.js
@@ -0,0 +1,24 @@
+// // Arrays are a list ordered of items
+
+// let featuredPosts = [
+//   "Check out my Netflix clone",
+//   "Here's the code for my project",
+//   "I've just relaunched my portfolio",
+// ];
+
+// for (let i = 0; i < featuredPosts.length; i++) {
+//   console.log(featuredPosts[i]);
+// }
+
+let messages = [
+  "Hey, how's it going?",
+  "I'm great, thank you! How about you?",
+  "All good. Been working on my portfolio lately.",
+];
+
+let newMessage = "Same here!";
+
+messages.push(newMessage);
+console.log(messages);
+messages.pop();
+console.log(messages);
