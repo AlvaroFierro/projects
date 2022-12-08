301ffe39b2e6b128efcdaefeaab5aa4e2befc93e
Alvaro Fierro <business.afierro@gmail.com>
2022-12-08 13:14:49 -0600

    Refactor: directory

--- scrimba/adventJS/tacoTray/index.js
+++
@@ -1,8 +0,0 @@
-const tacoTray = document.getElementById('tray')
-
-function getRandomNumberOfTacos() {
-  const emptyArray = ['Empty array']
-  const number = Math.floor(Math.random() * 10) + 1
-  tacoTray.innerHTML = number
-}
-getRandomNumberOfTacos()
