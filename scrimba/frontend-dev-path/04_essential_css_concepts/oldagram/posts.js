d380a61defc2af07e571c95abf5da73922120416
Alvaro Fierro <business.afierro@gmail.com>
2023-01-12 01:48:15 -0600

    Feat: posts added

---
+++ scrimba/frontend-dev-path/04_essential_css_concepts/oldagram/posts.js
@@ -0,0 +1,32 @@
+const posts = [
+  {
+    name: 'Vincent van Gogh',
+    username: 'vincey1853',
+    location: 'Zundert, Netherlands',
+    avatar: './img/avatar-vangogh.jpg',
+    post: './img/post-vangogh.jpg',
+    comment: 'just took a few mushrooms lol',
+    likes: 29851,
+  },
+  {
+    name: 'Gustave Courbet',
+    username: 'gus1819',
+    location: 'Ornans, France',
+    avatar: './img/avatar-courbet.jpg',
+    post: './img/post-courbet.jpg',
+    comment: "i'm feelin a bit stressed tbh",
+    likes: 25974,
+  },
+  {
+    name: 'Joseph Ducreux',
+    username: 'jd1735',
+    location: 'Paris, France',
+    avatar: './img/avatar-ducreux.jpg',
+    post: './img/post-ducreux.jpg',
+    comment:
+      'gm friends! which coin are YOU stacking up today?? post below and WAGMI!',
+    likes: 10592,
+  },
+]
+
+export default posts
