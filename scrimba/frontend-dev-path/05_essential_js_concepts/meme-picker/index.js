559142ba55bc2170bec15ea3fd29ba104f5d0e3b
Alvaro Fierro <business.afierro@gmail.com>
2023-01-20 23:43:13 -0600

    Feat:project finished

--- scrimba/frontend-dev-path/05_essential_js_concepts/meme-picker/index.js
+++ scrimba/frontend-dev-path/05_essential_js_concepts/meme-picker/index.js
@@ -1,13 +1,84 @@
 import { catsData } from './data'
 
-const emotionRadios = document.querySelector('.emotion-radios')
+const emotionRadios = document.getElementById('emotion-radios')
+const getImageBtn = document.getElementById('get-image-btn')
+const gifsOnlyOption = document.getElementById('gifs-only-option')
+const memeModal = document.getElementById('meme-modal')
+const memeModalInner = document.getElementById('meme-modal-inner')
+const memeModalCloseBtn = document.getElementById('meme-modal-close-btn')
+
+emotionRadios.addEventListener('change', highlightCheckedOption)
+getImageBtn.addEventListener('click', renderCat)
+memeModalCloseBtn.addEventListener('click', closeBtn)
+memeModalCloseBtn.addEventListener('mouseover', hoverBtn)
+
+function highlightCheckedOption(e) {
+  const radios = document.getElementsByClassName('radio')
+
+  for (let radio of radios) {
+    radio.classList.remove('highlight')
+  }
+
+  document.getElementById(e.target.id).parentElement.classList.add('highlight')
+}
+
+function renderCat() {
+  const catObject = getSingleCatObject()
+
+  memeModalInner.innerHTML = `
+  <img 
+  class="cat-img"
+  src="./images/${catObject.image}"
+  alt="${catObject.alt}"
+  />
+  `
+
+  memeModal.style.display = 'flex'
+}
+
+function closeBtn() {
+  memeModal.style.display = 'none'
+}
+
+function hoverBtn() {
+  memeModalCloseBtn.style.cursor = 'pointer'
+}
+
+function getSingleCatObject() {
+  const catsArray = getMatchingCatsArray()
+
+  if (catsArray.length === 1) {
+    return catsArray[0]
+  } else {
+    const randomObject = Math.floor(Math.random() * catsArray.length)
+    return catsArray[randomObject]
+  }
+}
+
+function getMatchingCatsArray() {
+  if (document.querySelector('input[type="radio"]:checked')) {
+    const selectedEmotion = document.querySelector('input[type="radio"]:checked').value
+    const isGif = gifsOnlyOption.checked
+
+    const matchingCatsArr = catsData.filter((cat) => {
+      if (isGif) {
+        return cat.emotionTags.includes(selectedEmotion) && cat.isGif
+      } else {
+        return cat.emotionTags.includes(selectedEmotion)
+      }
+    })
+    return matchingCatsArr
+  }
+}
 
 function getEmotionsArray(cats) {
   const emotionsArr = []
 
   for (let cat of cats) {
     for (let emotion of cat.emotionTags) {
-      emotionsArr.push(emotion)
+      if (!emotionsArr.includes(emotion)) {
+        emotionsArr.push(emotion)
+      }
     }
   }
 
@@ -30,21 +101,6 @@ function renderEmotionsRadios(cats) {
     </div>`
   }
 
-  /*
-    Challenge:
-    1. Swap out `<p>${emotion}</p>` for HTML
-      that will render a radio input for each
-      emotion. Remember to use "type", "id", 
-      "value", and "name" properties on each radio.
-      ("id" and "value" can both be set to the
-      "emotion").
-    2. Remember to give each radio a label.
-      (What property does a label need?)
-    3. Enclose each individual radio input in this div:
-      <div class="radio">
-          **RADIO HERE**
-      </div>
-*/
   emotionRadios.innerHTML = string
 }
 
