82c929b746087878c294c3ada42094f09dfeadc9
Alvaro Fierro <business.afierro@gmail.com>
2023-02-27 16:03:04 -0600

    Chessboard exercise added

---
+++ exercises/chessboard.js
@@ -0,0 +1,15 @@
+// create a chessboard that creates a string that represents
+// an 8x8 grid, using newline characters to separate lines.
+// At each position  of the grid there is either a space or
+// a '#' character. The characters should form a chessboard
+
+// should log something like this...
+
+//  # # # #
+// # # # #
+//  # # # #
+// # # # #
+//  # # # #
+// # # # #
+//  # # # #
+// # # # #
