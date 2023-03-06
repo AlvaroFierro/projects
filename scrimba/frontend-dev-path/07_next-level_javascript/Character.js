a136dcabb48e40e8071e35d66aa95a12b58b43af
Alvaro Fierro <business.afierro@gmail.com>
2023-03-05 23:30:14 -0600

    RPG modified

--- scrimba/frontend-dev-path/07_next-level_javascript/Character.js
+++ scrimba/frontend-dev-path/07_next-level_javascript/Character.js
@@ -1,28 +1,53 @@
-import { getDiceRollArray } from './utils.js'
+import { getDiceRollArray, getDicePlaceholderHtml, getPercentage } from './utils.js'
 
 function Character(data) {
   Object.assign(this, data)
 
-  const { name, avatar, health, diceCount } = this
+  // methods
+  this.maxHealth = this.health
 
-  this.getDiceHtml = function getDiceHtml(diceCount) {
-    return getDiceRollArray(diceCount)
-      .map(function (num) {
-        return `<div class="dice">${num}</div>`
-      })
-      .join('')
+  this.diceArray = getDicePlaceholderHtml(this.diceCount)
+
+  this.getDiceHtml = function (diceCount) {
+    this.currentDiceScore = getDiceRollArray(this.diceCount)
+
+    this.diceArray = this.currentDiceScore.map((num) => `<div class="dice">${num}</div>`).join('')
+  }
+
+  this.takeDamage = function (attackScoreArray) {
+    const totalAttackScore = attackScoreArray.reduce((total, current) => total + current)
+
+    this.health -= totalAttackScore
+    if (this.health <= 0) {
+      this.dead = true
+      this.health = 0
+    }
+    console.log(getPercentage(this.health, this.maxHealth))
+  }
+
+  this.getHealthBarHtml = function () {
+    const percent = getPercentage(this.health, this.maxHealth)
+
+    return `
+    <div class="health-bar-outer">
+      <div class="health-bar-inner ${percent <= 25 ? 'danger' : ''}" 
+          style="width:${percent}%;">
+      </div>
+    </div>`
   }
 
   this.getCharacterHtml = function () {
-    const diceHtml = this.getDiceHtml(diceCount)
+    const { name, avatar, health, diceCount, diceArray } = this
+    const healthBar = this.getHealthBarHtml()
 
     return `
     <div class="character-card">
-      <h4 class="name"> ${name} </h4>
-      <img class="avatar" src="${avatar}" />
-      <div class="health">health: <b> ${health} </b></div>
-      <div class="dice-container">${diceHtml}
-      </div>
+    <h4 class="name"> ${name} </h4>
+    <img class="avatar" src="${avatar}" />
+    <div class="health">health: <b> ${health} </b></div>
+    ${healthBar}
+    <div class="dice-container">${diceArray}
+    </div>
     </div>`
   }
 }
--- scrimba/frontend-dev-path/07_next-level_javascript/index.js
+++ scrimba/frontend-dev-path/07_next-level_javascript/index.js
@@ -1,6 +1,34 @@
 import characterData from './data.js'
 import Character from './Character.js'
 
+let monstersArray = ['orc', 'demon', 'goblin']
+
+function attack() {
+  wizard.getDiceHtml()
+  orc.getDiceHtml()
+  wizard.takeDamage(wizard.currentDiceScore)
+  orc.takeDamage(orc.currentDiceScore)
+  render()
+
+  if (wizard.dead || orc.dead) gameOver()
+}
+
+function gameOver() {
+  const endMessage =
+    orc.dead && wizard.dead
+      ? 'No victors - all creatures are dead'
+      : wizard.dead
+      ? 'The Orc is Victorius'
+      : 'The Wizard Wins'
+
+  const endEmoji = orc.dead ? '🔮' : '☠️'
+  document.body.innerHTML = `<div class="end-game">
+    <h2>Game Over</h2>
+    <h3>${endMessage}</h3>
+    <p class="end-emoji">${endEmoji}</p>
+  </div>`
+}
+
 const wizard = new Character(characterData.hero)
 const orc = new Character(characterData.monster)
 
@@ -8,4 +36,6 @@ function render() {
   document.getElementById('hero').innerHTML = wizard.getCharacterHtml()
   document.getElementById('monster').innerHTML = orc.getCharacterHtml()
 }
+
+document.getElementById('attack-button').addEventListener('click', attack)
 render()
