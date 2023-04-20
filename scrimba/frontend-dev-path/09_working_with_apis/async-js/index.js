0e1282344abb7234a8d7342d4ba4836d5b392b3b
Alvaro Fierro <business.afierro@gmail.com>
2023-04-19 23:22:17 -0600

    Feat: determine who win the game

--- scrimba/frontend-dev-path/09_working_with_apis/async-js/index.js
+++ scrimba/frontend-dev-path/09_working_with_apis/async-js/index.js
@@ -1,4 +1,8 @@
 let deckId = ''
+let computerScore = 0,
+  ownScore = 0
+const computerScoreEl = document.getElementById('computer-score')
+const ownScoreEl = document.getElementById('own-score')
 const cardsContainer = document.getElementById('deck')
 const newDeckBtn = document.getElementById('new-deck')
 const drawCardBtn = document.getElementById('draw-card')
@@ -16,6 +20,7 @@ function fetchCard() {
     .then((data) => {
       console.log(data)
       deckId = data.deck_id
+      remainingCardsEl.textContent = `Remaining cards: ${data.remaining}`
     })
 }
 
@@ -26,9 +31,20 @@ function drawCard() {
       cardsContainer.children[0].innerHTML = `<img src="${data.cards[0].image}">`
       cardsContainer.children[1].innerHTML = `<img src="${data.cards[1].image}">`
       const winnerTxt = determineWinner(data.cards[0], data.cards[1])
+
       winnerTxtEl.innerHTML = winnerTxt
-      const remainingCardsCount = `Remaining cards: ${data.remaining}`
-      remainingCardsEl.innerHTML = remainingCardsCount
+      remainingCardsEl.textContent = `Remaining cards: ${data.remaining}`
+
+      if (data.remaining === 0) {
+        drawCardBtn.disabled = true
+        if (computerScore > ownScore) {
+          winnerTxtEl.innerHTML = 'The computer won the game!'
+        } else if (computerScore < ownScore) {
+          winnerTxtEl.innerHTML = 'You won the game!'
+        } else {
+          winnerTxtEl.innerHTML = 'It is a tie game!'
+        }
+      }
     })
 }
 
@@ -38,9 +54,13 @@ function determineWinner(card1, card2) {
   const card2Value = valueOptions.indexOf(card2.value)
 
   if (card1Value > card2Value) {
-    return 'Card 1 wins'
+    computerScore++
+    computerScoreEl.textContent = `Computer score: ${computerScore}`
+    return 'Computer wins!'
   } else if (card1Value < card2Value) {
-    return 'Card 2 wins'
+    ownScore++
+    ownScoreEl.textContent = `Your score: ${ownScore}`
+    return 'You win!'
   } else {
     return 'War'
   }
